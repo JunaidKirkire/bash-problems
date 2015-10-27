ab51345f67fa8aa3dccc30defb12eb7194beb7b9
JunaidKirkire <junaidbkirkire@gmail.com>
2015-10-27 16:12:54 +0530

    Take log file name from command line argument

--- log_parser.sh
+++ log_parser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-awk -f clean_log_file.awk tmp_more_than_50
+awk -f clean_log_file.awk $1
 echo "The longest request is -"
 cat tmp.txt | pcregrep -o1 -o4 -i '((POST|GET) ".*"\s)(for.*in\s)(\d+)' | awk {'print $1 " "$2, $3"ms"'} | sort -n -r -k3 | head -1
 rm tmp.txt
